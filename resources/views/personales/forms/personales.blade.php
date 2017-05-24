48b7bce118c69588a45467ed3e5f1233a0a6203c
esolorzano <edison.solorzano19@gmail.com>
2017-05-24 15:35:39 -0500

    Cambio Sync

--- resources/views/personales/forms/personales.blade.php
+++ resources/views/personales/forms/personales.blade.php
@@ -1,28 +1,28 @@
 <div class="container">
 
 	<div class="form-group">	
-		<div class="col-lg-12 col-md-12 col-xs-12">
+		<div class="col-lg-6 col-md-12 col-xs-12">
 			{!!Form::label('persona_id','Persona:')!!}
-			{!!Form::select('persona_id', $personas, null, ['id'=>'persona_id', 'class'=>'form-control'])!!}
+			{!!Form::select('persona_id', $personas, null, ['id'=>'persona_id', 'class'=>'form-control', 'placeholder'=>'Seleccione una persona'])!!}
 		</div>
 	</div>
 
 	<div class="form-group">
-		<div class="col-lg-12 col-md-12 col-xs-12">
+		<div class="col-lg-6 col-md-12 col-xs-12">
 			{!!Form::label('FechaInicio','Fecha Inicio:')!!}
 			{!!Form::date('fecha_inicio', \Carbon\Carbon::now(), ['id' => 'fecha_inicio', 'class' => 'form-control'])!!}
 		</div>
 	</div>
 
 	<div class="form-group">
-		<div class="col-lg-12 col-md-12 col-xs-12">
+		<div class="col-lg-6 col-md-12 col-xs-12">
 			{!!Form::label('FechaFin','Fecha Fin:')!!}
 			{!!Form::date('fecha_fin', \Carbon\Carbon::now(), ['id' => 'fecha_fin', 'class' => 'form-control'])!!}
 		</div>
 	</div>
 
 	<div class="form-group">	
-		<div class="col-lg-12 col-md-12 col-xs-12">
+		<div class="col-lg-6 col-md-12 col-xs-12">
 			{!!Form::label('departamento_id','Departamento:')!!}
 			{!!Form::select('departamento_id', $departamentos, null, ['id'=>'departamento_id', 'class'=>'form-control'])!!}
 		</div>
