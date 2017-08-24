159fb17a123a309b393c48d32b31606a71672549
jhonyGuaman <jhonker2@hotmail.com>
2017-08-24 08:36:49 -0500

    scriptpersonas

--- public/js/scriptPersonas.js
+++ public/js/scriptPersonas.js
@@ -78,3 +78,95 @@ $("#actualizarPersona").click(function(){
 	});
 });
 
+$("#registroPersona").click(function(){
+	if($('#doc_identificacion').val() == "" 
+		&& $('#nombres').val() == "" 
+		&& $('#apellido_paterno').val() == ""
+		&& $('#estado_civil').val() == ""){
+		
+		var animate_in = 'lightSpeedIn',
+        animate_out = 'bounceOut';
+        	new PNotify({title: 'Alerta Faltan datos',text: 'Por favor! algunos campos estan vacios',
+                 type: 'error',delay: 2500,
+                 animate: {animate: true,in_class: animate_in,out_class: animate_out}
+        	});
+
+            $('#doc_identificacion').addClass('error');
+            $('#nombres').addClass('error');
+            $('#apellido_paterno').addClass('error');
+            $('#estado_civil').addClass('error');
+                
+  	}else if($('#doc_identificacion').val() == ""){
+        $('#doc_identificacion').addClass('error');
+        $('#span_doc_identificacion').addClass('error_span');
+        $('#span_mensaje_doc_identificacion').html('Ingrese el número de identificación');
+        var animate_in = 'lightSpeedIn', animate_out = 'bounceOut';
+        new PNotify({title: 'Alerta',text: 'Por favor! ingrese el número de identificación',
+	             type: 'error',delay: 2500,
+	             animate: {animate: true,in_class: animate_in,out_class: animate_out}
+        });
+
+    }else if($('#nombres').val() == ""){
+        $('#nombres').addClass('error');
+        $('#span_nombres').addClass('error_span');
+        $('#span_mensaje_nombres').html('Ingrese sus nombres');
+        var animate_in = 'lightSpeedIn', animate_out = 'bounceOut';
+        new PNotify({title: 'Alerta',text: 'Por favor! ingrese sus nombres',
+	             type: 'error',delay: 2500,
+	             animate: {animate: true,in_class: animate_in,out_class: animate_out}
+        });
+
+    }else if($('#apellido_paterno').val() == ""){
+        $('#apellido_paterno').addClass('error');
+        $('#span_apellido_paterno').addClass('error_span');
+        $('#span_mensaje_apellido_paterno').html('Ingrese su apellido paterno');
+        var animate_in = 'lightSpeedIn', animate_out = 'bounceOut';
+        new PNotify({title: 'Alerta',text: 'Por favor! ingrese su apellido paterno',
+	             type: 'error',delay: 2500,
+	             animate: {animate: true,in_class: animate_in,out_class: animate_out}
+        });
+
+    }else if($('#estado_civil').val() == ""){
+        $('#estado_civil').addClass('error');
+        $('#span_estado_civil').addClass('error_span');
+        $('#span_mensaje_estado_civil').html('Seleccione su estado civil');
+        var animate_in = 'lightSpeedIn', animate_out = 'bounceOut';
+        new PNotify({title: 'Alerta',text: 'Por favor! seleccione su estado civil',
+	             type: 'error',delay: 2500,
+	             animate: {animate: true,in_class: animate_in,out_class: animate_out}
+        });
+
+    }else{  
+      
+		registrarPersona();
+  	}
+});
+
+function registrarPersona(){ 
+  var detalle = []; // declaramos un array
+       $("input[type=checkbox]:checked").each(function(){
+        var obj = {'id_rol_persona': $(this).val(),} // creamos el obj de los id_rol_persona
+        detalle.push(obj); //añadimos el obj al array
+        });  
+  var detal= JSON.stringify(detalle); // convertimos en json el array
+
+	var datos = new FormData($("#frmPersonas")[0]);
+  datos.append('check',detal);
+	var route = "/personas"
+	var token = $("#token").val();
+
+	$.ajax({
+		url: route,
+		headers: {'X-CSRF-TOKEN': token},
+		type: 'POST',
+		dataType: 'json',	
+		contentType: false,
+		processData: false,		
+		data: datos,
+
+		success:function(){
+			$("#msj-insert-persona").fadeIn();
+		}
+	});
+}
+
